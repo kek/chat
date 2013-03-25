98606101bb6bf220d1480e0f068ed04720c9c1ac
Karl Eklund <localpart@gmail.com>
2013-03-25 20:51:32 +0100

    Server class

--- server.rb
+++ server.rb
@@ -10,8 +10,13 @@ $generator = Namey::Generator.new
 class User
   attr_reader :name
 
-  def initialize
+  def initialize socket
     @name = $generator.name(:rare)
+    @socket = socket
+  end
+
+  def message msg
+    @socket.send msg
   end
 end
 
@@ -21,38 +26,62 @@ class NoUser
   end
 end
 
-EM.run do
-  EM.start_server "0.0.0.0", 9090, WebPageServer
-
-  EM.start_server "127.0.0.1", 9091, ChatServer
-
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 9092, :debug => false) do |ws|
-    user = NoUser.new
-
-    ws.onopen do |handshake|
-      puts "WebSocket opened #{{
-        :path => handshake.path,
-        :query => handshake.query,
-        :origin => handshake.origin,
-      }}"
-      user = User.new
-
-      ws.send "Hello #{user.name}!"
-    end
-
-    ws.onmessage do |msg|
-      puts "Received message: #{msg}"
-      ws.send "#{user.name}: #{msg}"
-    end
-
-    ws.onclose do
-      puts "WebSocket closed"
-    end
-
-    ws.onerror do |e|
-      puts "Error: #{e.message}"
-    end
+class Chat
+  def initialize
+    @users = []
   end
 
-  puts "Running"
+  def add_user user
+    @users << user
+  end
+
+  def broadcast msg
+    @users.each do |user|
+      user.message msg
+    end
+  end
 end
+
+class Server
+  def initialize chat
+    @chat = chat
+  end
+
+  def run
+    EM.run do
+      EM.start_server "0.0.0.0", 9090, WebPageServer
+
+      EM::WebSocket.run(:host => "0.0.0.0", :port => 9092, :debug => false) do |ws|
+        user = NoUser.new
+
+        ws.onopen do |handshake|
+          puts "WebSocket opened #{{
+            :path => handshake.path,
+            :query => handshake.query,
+            :origin => handshake.origin,
+          }}"
+          user = User.new ws
+          @chat.add_user user
+
+          ws.send "Hello #{user.name}!"
+        end
+
+        ws.onmessage do |msg|
+          @chat.broadcast "#{user.name}: #{msg}"
+        end
+
+        ws.onclose do
+          puts "WebSocket closed"
+        end
+
+        ws.onerror do |e|
+          puts "Error: #{e.message}"
+        end
+      end
+
+      puts "Running"
+    end
+  end
+end
+
+Server.new(Chat.new).run
